a743e26eead0d5154cf0565556d750a9f440fcb0
Caio Rodrigues <rfcaio90@gmail.com>
2021-05-02 20:09:45 -0300

    add format validation to ISBN10 class

--- ISBN10.js
+++ ISBN10.js
@@ -1,7 +1,13 @@
+const VALID_ISBN10_FORMAT = /^\d{9}[0-9X]$/
+
 class ISBN10 {
   constructor(value) {
     if (typeof value !== 'string') {
       throw new Error('Invalid type.')
+    }
+
+    if (!VALID_ISBN10_FORMAT.test(value)) {
+      throw new Error('Invalid ISBN-10 format.')
     }
   }
 }
--- ISBN10.test.js
+++ ISBN10.test.js
@@ -2,7 +2,12 @@ const ISBN10 = require('./ISBN10')
 
 describe('ISBN10', () => {
   test('throws an error if a non string parameter is passed', () => {
-    expect(() => new ISBN10(1932698183)).toThrow(Error)
-    expect(() => new ISBN10(1932698183)).toThrow('Invalid type.')
+    expect(() => new ISBN10(1234567890)).toThrow(Error)
+    expect(() => new ISBN10(1234567890)).toThrow('Invalid type.')
+  })
+
+  test('throws an error if a string with invalid format is passed', () => {
+    expect(() => new ISBN10('123456789')).toThrow(Error)
+    expect(() => new ISBN10('123456789x')).toThrow('Invalid ISBN-10 format.')
   })
 })

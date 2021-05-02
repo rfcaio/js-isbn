e4ca3d8e2fc168a4e0cc62abb09f6b10d3890f3f
Caio Rodrigues <rfcaio90@gmail.com>
2021-05-02 19:53:42 -0300

    add type validation to ISBN10 class

--- ISBN10.js
+++ ISBN10.js
@@ -1,3 +1,9 @@
-class ISBN10 {}
+class ISBN10 {
+  constructor(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Invalid type.')
+    }
+  }
+}
 
 module.exports = ISBN10
--- ISBN10.test.js
+++ ISBN10.test.js
@@ -1,7 +1,8 @@
 const ISBN10 = require('./ISBN10')
 
 describe('ISBN10', () => {
-  test('ISBN10 class should be defined', () => {
-    expect(ISBN10).toBeDefined()
+  test('throws an error if a non string parameter is passed', () => {
+    expect(() => new ISBN10(1932698183)).toThrow(Error)
+    expect(() => new ISBN10(1932698183)).toThrow('Invalid type.')
   })
 })

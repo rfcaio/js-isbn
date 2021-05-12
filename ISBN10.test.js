97ba64d8790e77ff30f4d33fa83ab78cd6e470bf
Caio Rodrigues <rfcaio90@gmail.com>
2021-05-11 22:05:34 -0300

    refactor format validation test

--- ISBN10.test.js
+++ ISBN10.test.js
@@ -9,8 +9,22 @@ describe('ISBN10', () => {
   })
 
   test('throws an error if a string with invalid format is passed', () => {
-    expect(() => new ISBN10('123456789')).toThrow(Error)
-    expect(() => new ISBN10('123456789x')).toThrow('Invalid ISBN-10 format.')
+    const createISBN10LessThanTenCharacters = () => new ISBN10('123456789')
+
+    expect(createISBN10LessThanTenCharacters).toThrow(Error)
+    expect(createISBN10LessThanTenCharacters).toThrow('Invalid ISBN-10 format.')
+
+    const createISBN10GreaterThanTenCharacters = () => new ISBN10('0123456789X')
+
+    expect(createISBN10GreaterThanTenCharacters).toThrow(Error)
+    expect(createISBN10GreaterThanTenCharacters).toThrow(
+      'Invalid ISBN-10 format.'
+    )
+
+    const createISBN10WithLowerFinalX = () => new ISBN10('123456789x')
+
+    expect(createISBN10WithLowerFinalX).toThrow(Error)
+    expect(createISBN10WithLowerFinalX).toThrow('Invalid ISBN-10 format.')
   })
 
   test('throws an error if an invalid ISBN-10 is passed', () => {

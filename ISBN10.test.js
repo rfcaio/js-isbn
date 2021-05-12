68c94af5cc4fa8fd7f247910b8f72925c52ca19b
Caio Rodrigues <rfcaio90@gmail.com>
2021-05-11 21:56:27 -0300

    refactor type validation test

--- ISBN10.test.js
+++ ISBN10.test.js
@@ -2,8 +2,10 @@ const ISBN10 = require('./ISBN10')
 
 describe('ISBN10', () => {
   test('throws an error if a non string parameter is passed', () => {
-    expect(() => new ISBN10(1234567890)).toThrow(Error)
-    expect(() => new ISBN10(1234567890)).toThrow('Invalid type.')
+    const createISBN10WithInvalidType = () => new ISBN10(1234567890)
+
+    expect(createISBN10WithInvalidType).toThrow(Error)
+    expect(createISBN10WithInvalidType).toThrow('Invalid type.')
   })
 
   test('throws an error if a string with invalid format is passed', () => {

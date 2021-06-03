5c60aeb07a452bc5c7d1926385e1d41b598aae33
Caio Rodrigues <rfcaio90@gmail.com>
2021-06-02 21:15:13 -0300

    add format validation to ISBN-13 class

--- ISBN13.js
+++ ISBN13.js
@@ -1,14 +1,24 @@
 const InvalidISBN13Error = require('./InvalidISBN13Error')
 
+const VALID_ISBN13_FORMAT = /^\d{13}$/
+
 class ISBN13 {
   constructor(value) {
     if (this._hasInvalidType(value)) {
       throw new InvalidISBN13Error('Invalid type.')
     }
+
+    if (this._hasInvalidFormat(value)) {
+      throw new InvalidISBN13Error('Invalid ISBN-13 format.')
+    }
   }
 
   _hasInvalidType(value) {
     return typeof value !== 'string'
+  }
+
+  _hasInvalidFormat(value) {
+    return !VALID_ISBN13_FORMAT.test(value)
   }
 }
 
--- ISBN13.test.js
+++ ISBN13.test.js
@@ -8,4 +8,18 @@ describe('ISBN13', () => {
     expect(createISBN13WithInvalidType).toThrow(InvalidISBN13Error)
     expect(createISBN13WithInvalidType).toThrow('Invalid type.')
   })
+
+  test('throws an error if a string lower than thirteen characters is passed', () => {
+    const createInvalidISBN13 = () => new ISBN13('978855080460')
+
+    expect(createInvalidISBN13).toThrow(InvalidISBN13Error)
+    expect(createInvalidISBN13).toThrow('Invalid ISBN-13 format.')
+  })
+
+  test('throws an error if a string greater than thirteen characters is passed', () => {
+    const createInvalidISBN13 = () => new ISBN13('97885508046060')
+
+    expect(createInvalidISBN13).toThrow(InvalidISBN13Error)
+    expect(createInvalidISBN13).toThrow('Invalid ISBN-13 format.')
+  })
 })

3c824b34c5dde8f782acd9aa99ef6d22742c3bf3
Caio Rodrigues <rfcaio90@gmail.com>
2021-07-08 11:21:50 -0300

    change the names of helper functions of ISBN-13 tests

--- isbn13.test.js
+++ isbn13.test.js
@@ -3,40 +3,40 @@ const InvalidIsbn13Error = require('./invalid-isbn13-error')
 
 describe('Isbn13', () => {
   test('throws an error if a non string parameter is passed', () => {
-    const createISBN13WithInvalidType = () => new Isbn13(9788550804606)
+    const createIsbn13WithInvalidType = () => new Isbn13(9788550804606)
 
-    expect(createISBN13WithInvalidType).toThrow(InvalidIsbn13Error)
-    expect(createISBN13WithInvalidType).toThrow('Invalid type.')
+    expect(createIsbn13WithInvalidType).toThrow(InvalidIsbn13Error)
+    expect(createIsbn13WithInvalidType).toThrow('Invalid type.')
   })
 
   test('throws an error if a string lower than thirteen characters is passed', () => {
-    const createInvalidISBN13 = () => new Isbn13('978855080460')
+    const createInvalidIsbn13 = () => new Isbn13('978855080460')
 
-    expect(createInvalidISBN13).toThrow(InvalidIsbn13Error)
-    expect(createInvalidISBN13).toThrow('Invalid ISBN-13 format.')
+    expect(createInvalidIsbn13).toThrow(InvalidIsbn13Error)
+    expect(createInvalidIsbn13).toThrow('Invalid ISBN-13 format.')
   })
 
   test('throws an error if a string greater than thirteen characters is passed', () => {
-    const createInvalidISBN13 = () => new Isbn13('97885508046060')
+    const createInvalidIsbn13 = () => new Isbn13('97885508046060')
 
-    expect(createInvalidISBN13).toThrow(InvalidIsbn13Error)
-    expect(createInvalidISBN13).toThrow('Invalid ISBN-13 format.')
+    expect(createInvalidIsbn13).toThrow(InvalidIsbn13Error)
+    expect(createInvalidIsbn13).toThrow('Invalid ISBN-13 format.')
   })
 
   test('throws an error if an invalid ISBN-13 is passed', () => {
-    const createInvalidISBN13 = () => new Isbn13('9788550804600')
+    const createInvalidIsbn13 = () => new Isbn13('9788550804600')
 
-    expect(createInvalidISBN13).toThrow(InvalidIsbn13Error)
-    expect(createInvalidISBN13).toThrow('Invalid ISBN-13 code.')
+    expect(createInvalidIsbn13).toThrow(InvalidIsbn13Error)
+    expect(createInvalidIsbn13).toThrow('Invalid ISBN-13 code.')
   })
 
   test('not throws an error if a valid ISBN-13 is passed', () => {
-    const createValidISBN13 = () => new Isbn13('9788550804606')
-    expect(createValidISBN13).not.toThrow(InvalidIsbn13Error)
+    const createValidIsbn13 = () => new Isbn13('9788550804606')
+    expect(createValidIsbn13).not.toThrow(InvalidIsbn13Error)
 
-    const createValidISBN13WithZeroAsVerifyDigit = () =>
+    const createValidIsbn13WithZeroAsVerifyDigit = () =>
       new Isbn13('9788576059240')
-    expect(createValidISBN13WithZeroAsVerifyDigit).not.toThrow(
+    expect(createValidIsbn13WithZeroAsVerifyDigit).not.toThrow(
       InvalidIsbn13Error
     )
   })

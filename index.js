949a9f23363c12a9489f8c7a0db3541bdc89fd21
Caio Rodrigues <rfcaio90@gmail.com>
2021-07-08 11:33:57 -0300

    create index file

---
+++ index.js
@@ -0,0 +1,6 @@
+const InvalidIsbn10Error = require('./src/invalid-isbn10-error')
+const InvalidIsbn13Error = require('./src/invalid-isbn13-error')
+const Isbn10 = require('./src/isbn10')
+const Isbn13 = require('./src/isbn13')
+
+module.exports = { InvalidIsbn10Error, InvalidIsbn13Error, Isbn10, Isbn13 }

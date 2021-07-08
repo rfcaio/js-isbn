232c13754edcdb8799a1da32af2ff5962ba6eb55
Caio Rodrigues <rfcaio90@gmail.com>
2021-07-08 11:20:15 -0300

    change the names of helper functions of ISBN-10 tests

--- isbn10.test.js
+++ isbn10.test.js
@@ -3,46 +3,46 @@ const InvalidIsbn10Error = require('./invalid-isbn10-error')
 
 describe('Isbn10', () => {
   test('throws an error if a non string parameter is passed', () => {
-    const createISBN10WithInvalidType = () => new Isbn10(1234567890)
+    const createIsbn10WithInvalidType = () => new Isbn10(1234567890)
 
-    expect(createISBN10WithInvalidType).toThrow(InvalidIsbn10Error)
-    expect(createISBN10WithInvalidType).toThrow('Invalid type.')
+    expect(createIsbn10WithInvalidType).toThrow(InvalidIsbn10Error)
+    expect(createIsbn10WithInvalidType).toThrow('Invalid type.')
   })
 
   test('throws an error if a string lower than ten characters is passed', () => {
-    const createInvalidISBN10 = () => new Isbn10('123456789')
+    const createInvalidIsbn10 = () => new Isbn10('123456789')
 
-    expect(createInvalidISBN10).toThrow(InvalidIsbn10Error)
-    expect(createInvalidISBN10).toThrow('Invalid ISBN-10 format.')
+    expect(createInvalidIsbn10).toThrow(InvalidIsbn10Error)
+    expect(createInvalidIsbn10).toThrow('Invalid ISBN-10 format.')
   })
 
   test('throws an error if a string greater than ten characters is passed', () => {
-    const createInvalidISBN10 = () => new Isbn10('0123456789X')
+    const createInvalidIsbn10 = () => new Isbn10('0123456789X')
 
-    expect(createInvalidISBN10).toThrow(InvalidIsbn10Error)
-    expect(createInvalidISBN10).toThrow('Invalid ISBN-10 format.')
+    expect(createInvalidIsbn10).toThrow(InvalidIsbn10Error)
+    expect(createInvalidIsbn10).toThrow('Invalid ISBN-10 format.')
   })
 
   test('throws an error if a string with a small x at the end is passed', () => {
-    const createInvalidISBN10 = () => new Isbn10('123456789x')
+    const createInvalidIsbn10 = () => new Isbn10('123456789x')
 
-    expect(createInvalidISBN10).toThrow(InvalidIsbn10Error)
-    expect(createInvalidISBN10).toThrow('Invalid ISBN-10 format.')
+    expect(createInvalidIsbn10).toThrow(InvalidIsbn10Error)
+    expect(createInvalidIsbn10).toThrow('Invalid ISBN-10 format.')
   })
 
   test('throws an error if an invalid ISBN-10 is passed', () => {
-    const createInvalidISBN10 = () => new Isbn10('1932698184')
+    const createInvalidIsbn10 = () => new Isbn10('1932698184')
 
-    expect(createInvalidISBN10).toThrow(InvalidIsbn10Error)
-    expect(createInvalidISBN10).toThrow('Invalid ISBN-10 code.')
+    expect(createInvalidIsbn10).toThrow(InvalidIsbn10Error)
+    expect(createInvalidIsbn10).toThrow('Invalid ISBN-10 code.')
   })
 
   test('not throws an error if a valid ISBN-10 is passed', () => {
-    const createValidISBN10 = () => new Isbn10('1932698183')
-    expect(createValidISBN10).not.toThrow(InvalidIsbn10Error)
+    const createValidIsbn10 = () => new Isbn10('1932698183')
+    expect(createValidIsbn10).not.toThrow(InvalidIsbn10Error)
 
-    const createValidISBN10WithCapitalXAtEnd = () => new Isbn10('855080603X')
-    expect(createValidISBN10WithCapitalXAtEnd).not.toThrow(InvalidIsbn10Error)
+    const createValidIsbn10WithCapitalXAtEnd = () => new Isbn10('855080603X')
+    expect(createValidIsbn10WithCapitalXAtEnd).not.toThrow(InvalidIsbn10Error)
   })
 
   test('returns ISBN-10 instance value at string conversion', () => {
